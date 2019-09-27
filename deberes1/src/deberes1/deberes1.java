6733b9992dacec73bb9adfc65af944fab3d050e7
alumno <alumno@pc04.taller1.rf6>
2019-09-27 10:48:42 +0200

    Indicar en que posicion del string quiero estar y poder sacar por pantalla

---
+++ deberes1/src/deberes1/deberes1.java
@@ -0,0 +1,24 @@
+package deberes1;
+
+import java.util.Scanner;
+
+public class deberes1 {
+
+	public static void main(String[] args) {
+		String palabra1;
+		String palabra2;
+		Scanner scan= new Scanner(System.in);
+		System.out.println("Escribe la primera palabra: ");
+		palabra1= scan.next();
+		System.out.println("Escribe la segunda palabra: ");
+		palabra2= scan.next();
+		
+		System.out.println(palabra1.charAt(0)+ 
+				palabra1.substring(palabra1.length()-2, palabra1.length()));
+		
+		System.out.println(palabra2.charAt(0)+ 
+				palabra2.substring(palabra2.length()-2, palabra2.length()));
+
+	}
+
+}
