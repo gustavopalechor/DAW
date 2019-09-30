2c5d0f9e96db74c630c214d1550ee914b4325b01
Gustavo <gustavo.palechor@gmail.com>
2019-09-30 10:58:17 +0200

    Utilización de los strings

--- deberes1/src/deberes1/deberes1.java
+++ deberes1/src/deberes1/deberes1.java
@@ -7,18 +7,27 @@ public class deberes1 {
 	public static void main(String[] args) {
 		String palabra1;
 		String palabra2;
-		Scanner scan= new Scanner(System.in);
+		String palabra3;
+		String correo="";
+		boolean esCorreo=false;
+		palabra3 = "hola";
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Escribe la primera palabra: ");
-		palabra1= scan.next();
+		palabra1 = scan.next();
 		System.out.println("Escribe la segunda palabra: ");
-		palabra2= scan.next();
-		
-		System.out.println(palabra1.charAt(0)+ 
-				palabra1.substring(palabra1.length()-2, palabra1.length()));
-		
-		System.out.println(palabra2.charAt(0)+ 
-				palabra2.substring(palabra2.length()-2, palabra2.length()));
+		palabra2 = scan.next();
 
+		System.out.println(
+				palabra1.toUpperCase().charAt(0) + palabra1.substring(palabra1.length() - 2, palabra1.length()));
+
+		System.out.println(
+				palabra2.toUpperCase().charAt(0) + palabra2.substring(palabra2.length() - 2, palabra2.length()));
+		System.out.println(palabra3.toUpperCase().charAt(0)); // sacar una letra en mayuscula indicando la posicion que
+																// se encuentra
+		System.out.println("Me escribes tu correo"); //Utilizar el boolean para saber si es 
+		correo=scan.next();							//un correo electronico	
+		esCorreo=correo.contains("@");
+		System.out.println("es: "+ esCorreo + " correo");
 		scan.close();
 	}
 
