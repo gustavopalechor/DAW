deada9f71018dc6eaf6d1a34481d1558fb3f3922
Gustavo <gustavo.palechor@gmail.com>
2019-09-30 10:59:50 +0200

    Ejercicios clase

--- Palabras/src/palabras.java
+++ Palabras/src/palabras.java
@@ -14,6 +14,12 @@ public class palabras {
 		
 		System.out.println("La primera de cada palabra es: \n" + palabra.charAt(0)
 		+"\t" + palabra2.charAt(0));
+		
+		palabra= palabra.substring(1, 3);
+		palabra2= palabra.substring(palabra.length()-3,palabra.length());
+		
+		System.out.println(palabra + palabra2);
+		
 
 		scan.close();
 	}
