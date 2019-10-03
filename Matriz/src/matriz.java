a08d419088eacf2d4d1a7db53c64887e29f0dad3
Gustavo <gustavo.palechor@gmail.com>
2019-10-03 10:21:51 +0200

    Matriz

--- Matriz/src/matriz.java
+++ Matriz/src/matriz.java
@@ -2,8 +2,8 @@
 public class matriz {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 7; i++) {
-			for (int j = 0; j < 5; j++) {
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 6; j++) {
 				if (i == 0 || i == 6 || j == 0 || j == 4) {
 					System.out.print("*");
 
