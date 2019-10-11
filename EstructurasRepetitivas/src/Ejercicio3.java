56126e5b972dde8d5d7d06e045ace5b7a0fd917d
Gustavo <gustavo.palechor@gmail.com>
2019-10-11 12:34:17 +0200

    Ejercicios con while

---
+++ EstructurasRepetitivas/src/Ejercicio3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		String palabra;
+		int contador=0;
+		
+		Scanner scan= new Scanner(System.in);
+		System.out.println("Escribe una palabra");
+		palabra= scan.next();
+		
+		while (contador<palabra.length()) {
+			System.out.println(palabra.charAt(contador));
+			contador++;
+			
+			
+		}
+		scan.close();
+	}
+
+}
---
+++ EstructurasRepetitivas/src/elWhile.java
@@ -0,0 +1,17 @@
+
+public class elWhile {
+
+	public static void main(String[] args) {
+		int numero=7;
+		int contador=1;
+		
+		
+		while (contador<=20) {
+			System.out.println(numero+ "*" + contador + "= " + (numero*contador));
+			contador++;
+			numero= numero*2;
+		}
+
+	}
+
+}
