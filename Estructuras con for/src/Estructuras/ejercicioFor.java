0591c6fd59ed8649f8712bbff2163dc2fdc65c32
Gustavo <gustavo.palechor@gmail.com>
2019-10-15 23:12:45 +0200

    Ejercicio clase

---
+++ Estructuras con for/src/Estructuras/ejercicioFor.java
@@ -0,0 +1,58 @@
+package Estructuras;
+
+import java.util.Scanner;
+
+public class ejercicioFor {
+	
+	public static int resultado(String contraseña) {
+		char letra= ' ';
+		int opcion=0;
+		for(int i=0; i < contraseña.length(); i++) {
+			letra=contraseña.charAt(i);
+			if(contraseña.length()<8) {
+			
+			return 1;}
+			
+			if(letra>=33 && letra<=96) {
+				opcion=opcion + 2;
+			}
+			else if((contraseña.length()>=8) && (letra>=97 && letra<=122)) {
+				opcion= opcion + 1;
+			}
+			
+		}
+		return opcion;
+	}
+
+	public static void main(String[] args) {
+		String contraseña;
+		int solucion;
+		
+		Scanner scan= new Scanner(System.in);
+		
+		System.out.println("Introduce tu contraseña");
+		contraseña= scan.next();
+		
+		solucion = resultado(contraseña); 
+		
+		if(solucion==1) { System.out.println("Contraseña demasiado corta");}
+		else if (solucion>contraseña.length() && solucion<contraseña.length()+8) { System.out.println("Contraseña poco segura");}
+		else if (solucion>contraseña.length()+8) { System.out.println("Contraseña segura");}
+		/*case 1: System.out.println("Contraseña demasiado corta");
+			break;
+		case 2:
+		case 3:
+		case 4: System.out.println("Contraseña poco segura");
+		break;
+		case 5: 
+		
+		
+		default:
+			break;
+		}*/	
+			
+
+		scan.close();
+	}
+
+}
