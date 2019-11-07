5d980b8fc963ed548e57a87d9d261a4e6cb53f8c
Gustavo <gustavo.palechor@gmail.com>
2019-11-07 09:21:26 +0100

    dibujar figuras

---
+++ OriendatoObjetos/src/ejercicioCuadrado/triangulo.java
@@ -0,0 +1,13 @@
+package ejercicioCuadrado;
+
+public class triangulo {
+
+	public static void main(String[] args) {
+		int base, altura=1;
+		for(int i = 0; i<altura; i++) {
+			System.out.println("*");
+		}
+
+	}
+
+}
