7d09a0ed711b2553ee565fec1ae3fc8535cb6653
gustavopalechor <gustavo.palechor@gmail.com>
2019-11-21 14:16:27 +0100

    Ejercicio clase Math

---
+++ OriendatoObjetos/src/clasesEspeciales/claseMath.java
@@ -0,0 +1,18 @@
+package clasesEspeciales;
+
+public class claseMath {
+
+	public static void main(String[] args) {
+		int numero= 5;
+		double numero1= 5.76;
+		
+		System.out.println(Math.exp(numero1));
+		System.out.println(Math.sin(numero));
+		System.out.println(Math.pow(numero, 2));
+		System.out.println(Math.round(numero1));
+		System.out.println(Math.floor(numero1));
+		System.out.println(Math.ceil(numero1));
+		System.out.println(Math.pow(2.71828, numero1));
+	}
+
+}
