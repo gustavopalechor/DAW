539e668e593d085d4a883a325a0555906807417b
gustavopalechor <gustavo.palechor@gmail.com>
2020-01-09 14:16:35 +0100

    Expresiones regulares

--- OriendatoObjetos/src/expresionesRegulares/ExpresionesRegulares.java
+++ OriendatoObjetos/src/expresionesRegulares/ExpresionesRegulares.java
@@ -11,16 +11,30 @@ public class ExpresionesRegulares {
 		Scanner scan= new Scanner(System.in);
 		String palabra;
 		String palabra1;
-		String control="(?i)[a-z]{2,}[@]{1}(gmail|hotmail)[.]{1}(com|es|net)";
-		System.out.println("Introduce correo electronico:");
-		palabra=scan.next();
-		System.out.println(Pattern.matches(control, palabra));
-
+		
+		  String control="(?i)[a-z]{2,}@(gmail|hotmail)[.]{1}(com|es|net)";
+		  System.out.println("Introduce correo electronico:"); palabra=scan.next();
+		  System.out.println(Pattern.matches(control, palabra)); System.out.println();
+		  
+		  String palabra2; String control2="([a-z]|\\d){1,}";
+		  System.out.println("Introduce una palabra con minusculas o numeros");
+		  palabra2=scan.next(); System.out.println(Pattern.matches(control2,
+		  palabra2));
+		  
+		  System.out.println();
+		  
+		  String palabra3; String control3="[a-zA-Z]{2,}\\w{0,}";
+		  System.out.println("Introduce una palabra con minusculas o numeros");
+		  palabra3=scan.next(); System.out.println(Pattern.matches(control3,
+		  palabra3));
+		 
+		
 		System.out.println();
-		String control1="[A-Z]{2}[a-z]{1,}[ ]{1}[a-z]{1,}[@]{1}(gmail|hotmail)[.]{1}(com|es|net)";
+		String control1="[A-Z]{2}[a-z]{1,}\\s{1}[a-z]{1,}[@]{1}(gmail|hotmail)[.]{1}(com|es|net)";
 		System.out.println("Introduce correo electronico:");
-		palabra1=scan.next();
+		palabra1=scan.nextLine();
 		System.out.println(Pattern.matches(control1, palabra1));
+		scan.close();
 	}
 
 }
