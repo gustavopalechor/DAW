c43ebdf3075765f4f43cbb389930dc099966abbd
gustavopalechor <gustavo.palechor@gmail.com>
2020-01-31 12:53:52 +0100

    hello word

--- Hello word/src/helloWord.java
+++ Hello word/src/helloWord.java
@@ -1,6 +1,8 @@
-
+import java.lang.System;
 public class helloWord {
 	public static void main(String[] args) {
 		System.out.println("Hello word");
+		System.out.println();
+		System.out.println("\u05D0");
 	}
 }
