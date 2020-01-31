9b2f20e7c017c0edaeb1f9641f4e431a79954ca5
gustavopalechor <gustavo.palechor@gmail.com>
2020-01-31 12:48:41 +0100

    Collection Map

--- OriendatoObjetos/src/Collections/Maps/ColeccionMap.java
+++ OriendatoObjetos/src/Collections/Maps/ColeccionMap.java
@@ -11,9 +11,7 @@ import java.util.Set;
 
 public class ColeccionMap {
 	
-	class Persona() {
-		
-	}
+
 
 	public static void main(String[] args) {
 		Map <Integer, String> coleccion= new HashMap<Integer, String>();
