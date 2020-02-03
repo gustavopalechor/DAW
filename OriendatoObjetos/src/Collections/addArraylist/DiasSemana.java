3f05909962874bb34f26d7ce938999c959f4c7fb
gustavopalechor <gustavo.palechor@gmail.com>
2020-02-03 12:22:56 +0100

    Collections

---
+++ OriendatoObjetos/src/Collections/addArraylist/DiasSemana.java
@@ -0,0 +1,18 @@
+package Collections.addArraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DiasSemana {
+	List<String> Dias;
+
+	
+
+	public DiasSemana() {
+		Dias= new ArrayList<String>(Arrays.asList("Lunes", "Martes", "Miercoles"));
+	}
+	
+	
+	
+}
