3806307fca2d03d52657ea8dd142d571436e6a8b
gustavopalechor <gustavo.palechor@gmail.com>
2020-02-20 14:14:37 +0100

    Files Quijote

--- Files/src/ejercicioQuijote/Quijote.java
+++ Files/src/ejercicioQuijote/Quijote.java
@@ -11,57 +11,67 @@ import java.io.IOException;
 
 public class Quijote {
 	
-	static public int contar(int contador) {
-		contador++;
-		return contador;
-	}
-	
-	
-	
-	static public void escribir(byte[] lista, int contador) {
-		for(byte s: lista) {
-			
-			System.out.print((char) s);
-			contar(contador);
-			if(s=='\n') {
-				System.out.print(contar(contador));
-				
-			}
+	static public void escribir(String palabra) {
+		
+		
+		
+			System.out.println(palabra+ palabra.length());
 			
 		}
-	}
-
+			
 	public static void main(String[] args) {
-		//Path paths= Paths.get("ficheros/Quijote.txt");
-		//InputStream stream= null;
-		/*
-		 * byte[] fichero; Charset charset= StandardCharsets.UTF_8;
-		 * 
-		 * try { fichero= Files.readAllBytes(paths);
-		 * 
-		 * for(byte s: fichero) {
-		 * 
-		 * System.out.print((char)s); if(s=='.') {
-		 * 
-		 * System.out.print("\n"); } }
-		 * 
-		 * } catch (Exception e) { e.getMessage(); }
-		 */
+		Path paths= Paths.get("ficheros/Quijote.txt");
+		InputStream stream= null;
 		
+		  byte[] fichero; Charset charset= StandardCharsets.UTF_8;
+		  
+		  try { fichero= Files.readAllBytes(paths);
+		  
+		  for(byte s: fichero) {
+		  
+		  System.out.print((char)s); if(s=='.') {
+		  
+		  System.out.print("\n"); } }
+		  
+		  } catch (Exception e) { e.getMessage(); }
+		  finally {
+			try {
+				stream.close();
+			} catch (Exception e2) {
+				e2.getMessage();
+			}
+		}		 
+		 /*******************************************/
+		System.out.println("\n\n");
+		  
+		  
 		byte[] lista = new byte[1];
-		int j=0;
-		int contador=0;
+		
+		String palabra="";
+		
 		RandomAccessFile ran1=null;
 		try {
 			ran1 = new RandomAccessFile("ficheros/Quijote.txt", "r");
 			for(int i=0; i<ran1.length(); i++) {
-				ran1.seek(j);
+				ran1.seek(i);
 				ran1.read(lista);
-				escribir(lista, contador);
-				j++;
+				
+				palabra+=(char)lista[0];
+				if(lista[0]=='\n') {
+				System.out.print((palabra.length()-1)+palabra);
+				palabra="";
+				}
+				
 			}
 		} catch (Exception e) {
 			e.getMessage();
+		}
+		finally {
+			try {
+				ran1.close();
+			} catch (Exception e2) {
+				e2.getCause();
+			}
 		}
 		
 		
